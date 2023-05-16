e6f6a9a198f77eba2c3341e07b63351154e5963a
Cody <codybarker.or@gmail.com>
2023-05-16 13:23:26 -0700

    s

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,17 +5,19 @@ class TicTacToe
         [3,4,5],
         [6,7,8],
         [0,3,6],
-        [1,4,3],
+        [1,4,7],
         [2,5,8],
         [0,4,8],
         [2,4,6]
     ]
+
+   
 
     def initialize
         @board = [" "," "," "," "," "," "," "," "," "]
     end
 
     def display_board
-        
+        @board.each {|spot| puts spot}
     end
 end
